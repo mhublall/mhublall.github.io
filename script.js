74796d254762ce0649d0c1738845e7358a45774d
Hublall, Mark <Mark.Hublall@tylertech.com>
2025-02-15 18:23:16 -0600

    initial push

---
+++ script.js
@@ -0,0 +1,7 @@
+function openTab() {
+    let tabcontent;
+    tabcontent = document.getElementById("tabcontent");
+    tabcontent.style.display = "block";
+}
+
+/* Javascript code adds functionality. The third flag is: magician  */
